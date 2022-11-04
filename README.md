96c8873006b84187ba4659bc08c794a37913b833
lzq <18418105@qq.com>
2022-11-04 13:19:27 +0800

    create yii2jajaxcrid gii

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# yii2jajaxcrud
-Gii CRUD template for Single Page Ajax Administration for yii2
+yii2jajaxcrud 
+=============
+
+[![Latest Stable Version](https://poser.pugx.org/jinowom/yii2jajaxcrud/v/stable)](https://packagist.org/packages/jinowom/yii2jajaxcrud)
+[![License](https://poser.pugx.org/jinowom/yii2jajaxcrud/license)](https://packagist.org/packages/jinowom/yii2jajaxcrud)
+[![Total Downloads](https://poser.pugx.org/jinowom/yii2jajaxcrud/downloads)](https://packagist.org/packages/jinowom/yii2jajaxcrud)
+
+Gii CRUD template for Single Page Ajax Administration for yii2 
+
+
+Features
+------------
++ Create, read, update, delete in onpage with Ajax
++ Bulk delete suport
++ Pjax widget suport
++ Export function(pdf,html,text,csv,excel,json)
++ Editable suport
++ Daterange suport
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist jinowom/yii2jajaxcrud "^2.1"
+or
+php composer.phar require --prefer-dist jinowom/yii2jajaxcrud "^3.0"
+```
+
+or add
+
+```
+"jinowom/yii2jajaxcrud": "^2.1"
+or
+"jinowom/yii2jajaxcrud": "^3.0"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+For first you must enable Gii module Read more about [Gii code generation tool](http://www.yiiframework.com/doc-2.0/guide-tool-gii.html)
+
+Because this extension used [kartik-v/yii2-grid](https://github.com/kartik-v/yii2-grid) extensions so we must config gridview module before
+
+Let 's add into modules config in your main config file
+````php
+'modules' => [
+    'gridview' =>  [
+        'class' => '\kartik\grid\Module'
+    ]       
+]
+````
+
+gii config like
+````php
+$config['bootstrap'][] = 'gii';
+$config['modules']['gii'] = [
+    'class' => 'yii\gii\Module',
+];
+$config['modules']['gii']['generators']['jinowommodel'] = [
+    'class' => \jinowom\jajaxcrud\generators\model\Generator::class,
+    'showName' => "YOUR MODEL GENERATOR",
+];
+$config['modules']['gii']['generators']['jinowomjajaxcrud'] = [
+    'class' => \jinowom\jajaxcrud\generators\crud\Generator::class,
+    'showName' => "YOUR AJAX CRUD GENERATOR",
+];
+````
+
+You can then access Gii through the following URL:
+
+http://localhost/path/to/index.php?r=gii
+
+and you can see <b>YOUR AJAX CRUD GENERATOR</b>
+
+![exp1](https://i.loli.net/2019/05/09/5cd3a7c2cb95a.png)
+![exp2](https://i.loli.net/2019/05/09/5cd3a7c2cee7a.png)
+![exp3](https://i.loli.net/2019/05/09/5cd3a7c2d14a9.png)
---
+++ src/generators/crud/default/search.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * This is the template for generating CRUD search class of the specified model.
+ */
+
+use yii\helpers\StringHelper;
+
+
+/* @var $this yii\web\View */
+/* @var $generator \jinowom\jajaxcrud\generators\crud\Generator */
+
+$modelClass = StringHelper::basename($generator->modelClass);
+$searchModelClass = StringHelper::basename($generator->searchModelClass);
+if ($modelClass === $searchModelClass) {
+    $modelAlias = $modelClass . 'Model';
+}
+$rules = $generator->generateSearchRules();
+$labels = $generator->generateSearchLabels();
+$editableFields = $generator->generateEditableFields();
+$thumbImageFields = $generator->generateThumbImageFields();
+foreach ($editableFields as $k => $v) {
+    $editableFields[$k] = "'{$v}'";
+}
+$searchAttributes = $generator->getSearchAttributes();
+$searchConditions = $generator->generateSearchConditions();
+
+$pks = $generator->modelClass::primaryKey();
+if ($generator->isDesc){
+    $defaultSort = "SORT_DESC";
+}else{
+    $defaultSort = "SORT_ASC";
+}
+if (count($pks) === 1) {
+    $pk = $pks[0];
+    $defaultOrder = "['$pk' => $defaultSort, ]";
+} else {
+    $defaultOrder = "[";
+    foreach ($pks as $pk) {
+        $defaultOrder .= "'$pk' => $defaultSort, ";
+    }
+    $defaultOrder .= "]";
+}
+
+echo "<?php\n";
+?>
+
+namespace <?= StringHelper::dirname(ltrim($generator->searchModelClass, '\\')) ?>;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use yii\db\ActiveQuery;
+use yii\db\Expression;
+use yii\helpers\ArrayHelper;
+use <?= ltrim($generator->modelClass, '\\') . (isset($modelAlias) ? " as $modelAlias" : "") ?>;
+
+/**
+ * <?= $searchModelClass ?> represents the model behind the search form about `<?= $generator->modelClass ?>`.
+ */
+class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $modelClass ?>
+
+{
+    const EMPTY_STRING = "(空字符)";
+    const NO_EMPTY = "(非空)";
+    const SCENARIO_EDITABLE = 'editable';
+
+    public function scenarios()
+    {
+        return ArrayHelper::merge(parent::scenarios(), [
+            self::SCENARIO_EDITABLE => [<?=implode(',', $editableFields) ?>],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            <?= implode(",\n            ", $rules) ?>,
+        ];
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     * @param array $params
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = self::find();
+        $this->load($params);
+        <?= implode("        ", $searchConditions) ?>
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            <?php if($generator->isDesc): ?>'sort' => ['defaultOrder' => <?=$defaultOrder ?>],
+            <?php else: ?>'sort' => ['defaultOrder' => <?=$defaultOrder ?>],
+        <?php endif; ?>]);
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+        return $dataProvider;
+    }
+
+    /**
+     * @param ActiveQuery $query
+     * @param $attribute
+     */
+    protected function _rangeFilter(&$query, $attribute, $isDate = false)
+    {
+        if ( ! is_null($this->$attribute) && strpos($this->$attribute, ' - ') !== false ) {
+            list($s, $e) = explode(' - ', $this->$attribute);
+            if ($isDate){
+                $s = strtotime($s);
+                $e = strtotime($e);
+            }
+            if ($s)$query->andFilterWhere(['>=', $attribute, $s]);
+            if ($e)$query->andFilterWhere(['<=', $attribute, $e]);
+        }
+    }
+
+    /**
+     * @param ActiveQuery $query
+     * @param $attribute
+     */
+    protected function _fieldFilter(&$query, $field, $attribute, $filter_type)
+    {
+        $this->$attribute = trim($this->$attribute);
+        switch($this->$attribute){
+            case \Yii::t('yii', '(not set)'):
+                $query->andFilterWhere(['IS', $field, new Expression('NULL')]);
+                break;
+            case self::EMPTY_STRING:
+                $query->andWhere([$field => '']);
+                break;
+            case self::NO_EMPTY:
+                $query->andWhere(['IS NOT', $field, new Expression('NULL')])->andWhere(['<>', $field, '']);
+                break;
+            default:
+                $query->andFilterWhere([$filter_type, $field, $this->$attribute]);
+                break;
+        }
+    }
+}
